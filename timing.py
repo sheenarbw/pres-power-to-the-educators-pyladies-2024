150b740a74d2d3aae9b1c272f120c7c4f6e7ba9b
Sheena <sheena.oconnell@gmail.com>
2024-09-18 09:34:34 +0200

    polish

---
+++ timing.py
@@ -0,0 +1,15 @@
+timing = """
+me 2:20
+everyone is an expert 3:00
+normal classroom  xxx  5:00
+bloom: 6:40
+mbl-> master teaching  1:40
+filling of a pail
+growth mindset
+framework 
+fundamental code concepts
+protege effect 
+guild 
+homework
+end 
+"""
