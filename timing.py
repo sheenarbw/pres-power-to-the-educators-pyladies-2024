d58a0b3a73265cfa343f640d2aa953afef7d36be
Sheena <sheena.oconnell@gmail.com>
2024-11-07 15:12:24 +0200

    progress

--- timing.py
+++ timing.py
@@ -1,42 +1,41 @@
+# timing = """
+# me 2:45
+# everyone is an expert 5:00
+# normal classroom  3:50
+# bloom: 6:40
+# mbl-> master teaching  1:00
+# filling of a pail: 4:30
+# growth mindset
+# framework
+# fundamental code concepts
+# protege effect
+# guild
+# homework
+# end
+# """
+
+# import re
+
+
+# def get_timing(timing):
+#     timing = timing.split("\n")
+#     timing = [re.sub(r"\d+:\d+", "", t) for t in timing]
+#     timing = [t for t in timing if t]
+#     return timing
+
+
 timing = """
 me 2:45
-everyone is an expert 5:00
 normal classroom  3:50
-bloom: 6:40
+bloom: 5:00
 mbl-> master teaching  1:00
-filling of a pail: 4:30
-growth mindset
-framework 
-fundamental code concepts
-protege effect 
-guild 
-homework
-end 
-"""
-
-import re
-
-
-def get_timing(timing):
-    timing = timing.split("\n")
-    timing = [re.sub(r"\d+:\d+", "", t) for t in timing]
-    timing = [t for t in timing if t]
-    return timing
-
-
-timing = """
-me 2:45
-normal classroom  3:50
-bloom: 6:40
-mbl-> master teaching  1:00
-filling of a pail: 4:30
-growth mindset: 5:00
+filling of a pail: 4:30  
+# growth mindset: 5:00
 framework: 3:30
 assess 7:00
-protege effect 1:38
+# protege effect 1:38
 guild 3:27
 homework
-end 
 """
 
 import re
@@ -47,6 +46,8 @@ def get_timing():
     total_seconds = 0
     for line in lines:
         if not line:
+            continue
+        if line.startswith("#"):
             continue
         found = re.search(r"(\d+):(\d+)", line)
         if found:
